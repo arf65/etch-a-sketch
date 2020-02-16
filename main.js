53cb8ff0b9b6b6950f9a16b59673fdc1767fbb08
Ben Redd <bentheredd13@gmail.com>
2020-02-16 03:38:29 -0800

    got a basic reset button working

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-for (let i = 1; i < 257; i++) {
-	const container = document.querySelector('#gridContainer');
-	const divs = document.createElement('div');
-	divs.classList.add('div' + i);
-	divs.classList.add('allDivs');
-	divs.style.border = '1px solid grey';
-	divs.addEventListener('mouseover', () => {
-		divs.style.backgroundColor = 'black';
-	});
+const container = document.querySelector('#gridContainer');
+initializeGrid();
 
-	container.appendChild(divs);
+function initializeGrid() {
+	for (let i = 0; i < 256; i++) {
+		const divs = document.createElement('div');
+		divs.classList.add('allDivs');
+		divs.style.border = '1px solid grey';
+		divs.addEventListener('mouseover', () => {
+			divs.style.backgroundColor = 'black';
+		});
+
+		container.appendChild(divs);
+	}
 }
 
 const button = document.querySelector('button');
@@ -16,8 +19,11 @@ button.addEventListener('click', reset);
 
 function reset() {
 	console.log('reset button clicked');
-	const allDivs = document.querySelector('.allDivs');
-	allDivs.style.backgroundColor = 'white';
+	allDivs = document.querySelectorAll('.allDivs');
+	for (i = 0; i < allDivs.length; i++) {
+		container.removeChild(allDivs[i]);
+	}
+	initializeGrid();
 }
 
 // function hoverEffect(square) {
